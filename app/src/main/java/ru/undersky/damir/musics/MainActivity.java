34a8e8832cd06307c3831ebe14cb46ac8462dd4a
Monster155 <damir180501@mail.ru>
2018-03-30 23:28:41 +0300

    DB Browser for SQLite

--- app/src/main/java/ru/undersky/damir/musics/MainActivity.java
+++ app/src/main/java/ru/undersky/damir/musics/MainActivity.java
@@ -1,102 +1,80 @@
 package ru.undersky.damir.musics;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextClock;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     ListView lv;
-    Button btn;
-    public static int length = 20;
-    MyArrayAdapter maa;
-    Context context = this;
-    boolean flag = true;
-    Model[] modelItems = new Model[length];
-    SharedPreferences mSettings;
+    TextView textView;
+    private DBHelper dbh;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSettings = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        textView = findViewById(R.id.textView);
+
+        dataB();
+        //listV();
+    }
+
+    public void dataB(){
+        dbh = new DBHelper(this);
+        try {dbh.updateDataBase();
+        } catch (IOException mIOException) {throw new Error("UnableToUpdateDatabase");}
+
+        try { db = dbh.getWritableDatabase();
+        } catch (SQLException mSQLException) {throw mSQLException;}
+
+
+        String product = "";
+
+        Cursor cursor = db.rawQuery("SELECT * FROM songs", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            product += cursor.getString(2) + " | ";
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        textView.setText(product);
+    }
+
+    public void listV(){
+        lv = findViewById(R.id.listView);
 
         String[] songs = getResources().getStringArray(R.array.namesOfSongs);
         String[] autors = getResources().getStringArray(R.array.autorsOfSongs);
-        for (int i = 0; i < length; i++)
-            if (mSettings.contains(songs[i] + " - " + autors[i])) {
-                modelItems[i] = new Model(songs[i] + " - " + autors[i],
-                        mSettings.getBoolean(songs[i] + " - " + autors[i], true));
-                Log.e("While", modelItems[i].getName() + " " + modelItems[i].getValue());
-            } else modelItems[i] = new Model(songs[i] + " - " + autors[i], false);
+        String[] all = new String[songs.length];
+        for(int i = 0; i < all.length; i++)
+            all[i] = songs[i] + " - " + autors[i];
 
-        maa = new MyArrayAdapter(this, modelItems);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, all);
 
-        btn = findViewById(R.id.button);
-        lv = findViewById(R.id.listView);
-        lv.setAdapter(maa);
+        lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
                 Intent i = new Intent(MainActivity.this, SecondActivity.class);
-                i.putExtra("k", position);
+                i.putExtra( "k", position);
                 startActivity(i);
             }
         });
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (flag) {
-                    btn.setText("Сортировка по избранным");
-                    flag = false;
-                } else {
-                    btn.setText("Сортировка по алфавиту");
-                    flag = true;
-                }
-                ArrayList<Model> selectedTeams = new ArrayList<>();
-                final SparseBooleanArray checkedItems = lv.getCheckedItemPositions();
-                int checkedItemsCount = checkedItems.size();
-                for (int i = 0; i < checkedItemsCount; i++) {
-                    // Item position in adapter
-                    int position = checkedItems.keyAt(i);
-                    // Add team if item is checked == TRUE!
-                    if (checkedItems.valueAt(i))
-                        selectedTeams.add(maa.getItem(position));
-                }
-                if (selectedTeams.size() < 2)
-                    Toast.makeText(context, "Need to select two or more teams.", Toast.LENGTH_SHORT);
-                else {
-                    // Just logging the output.
-                    for (Model m : selectedTeams)
-                        Log.e("Selected model: ", m.getName());
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        /*Model[] models = maa.getNames();
-        SharedPreferences.Editor editor = mSettings.edit();
-
-        for(int i = 0; i < length; i++) {
-            Log.e("onStop", models[i].getName()+" "+models[i].getValue());
-            editor.putBoolean(models[i].getName(), models[i].getValue());
-            editor.apply();
-        }
-        Log.e("onStop", "Saving end");*/
     }
 }
