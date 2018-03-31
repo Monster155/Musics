59a5ebfe66bce2b4a14904899b21010e1e9e7dad
Monster155 <damir180501@mail.ru>
2018-03-31 21:17:09 +0300

    DB Browser for SQLite

--- app/src/main/java/ru/undersky/damir/musics/DBHelper.java
+++ app/src/main/java/ru/undersky/damir/musics/DBHelper.java
@@ -14,7 +14,7 @@ import java.io.OutputStream;
 public class DBHelper extends SQLiteOpenHelper {
     private static String DB_NAME = "musics.db";
     private static String DB_PATH = "";
-    private static final int DB_VERSION = 3;
+    private static final int DB_VERSION = 1;
 
     private SQLiteDatabase mDataBase;
     private final Context mContext;
--- app/src/main/java/ru/undersky/damir/musics/MainActivity.java
+++ app/src/main/java/ru/undersky/damir/musics/MainActivity.java
@@ -10,15 +10,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextClock;
-import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     ListView lv;
-    TextView textView;
+    ArrayList<String> names = new ArrayList<>();
     private DBHelper dbh;
     private SQLiteDatabase db;
 
@@ -27,10 +26,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
-
         dataB();
-        //listV();
+        listV();
     }
 
     public void dataB(){
@@ -41,31 +38,20 @@ public class MainActivity extends Activity {
         try { db = dbh.getWritableDatabase();
         } catch (SQLException mSQLException) {throw mSQLException;}
 
-
-        String product = "";
-
         Cursor cursor = db.rawQuery("SELECT * FROM songs", null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            product += cursor.getString(2) + " | ";
+            names.add(cursor.getString(1) + " - " + cursor.getString(0));
             cursor.moveToNext();
         }
         cursor.close();
-
-        textView.setText(product);
     }
 
     public void listV(){
         lv = findViewById(R.id.listView);
 
-        String[] songs = getResources().getStringArray(R.array.namesOfSongs);
-        String[] autors = getResources().getStringArray(R.array.autorsOfSongs);
-        String[] all = new String[songs.length];
-        for(int i = 0; i < all.length; i++)
-            all[i] = songs[i] + " - " + autors[i];
-
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, all);
+                android.R.layout.simple_list_item_1, names);
 
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
